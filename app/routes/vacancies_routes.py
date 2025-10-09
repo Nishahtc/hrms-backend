cc49097a6456658225550c9dac28436233cb780e
Nishahtc <nishamodi254@email.com>
2025-10-09 13:12:08 +0530

    vacancies get route changes

--- app/routes/vacancies_routes.py
+++ app/routes/vacancies_routes.py
@@ -4,13 +4,29 @@ from app.config.config import VACANCY_FILE, IMAGE_URL
 
 router = APIRouter()
 
+# Get all vacancies
 @router.get("/")
 def get_vacancies():
-    data = read_data(VACANCY_FILE)
-    for vacancy in data:
-        if "img" in vacancy:
-            vacancy["img"] = f"{IMAGE_URL}/{vacancy['img']}"
-    return data
+    vacancies = read_data(VACANCY_FILE)  
+
+    for vac in vacancies:
+        if "img" in vac:
+            vac["img"] = f"{IMAGE_URL}/{vac['img']}"
+    return vacancies
+
+# Get vacancy by ID
+@router.get("/{vacancy_id}")
+def get_vacancy_by_id(vacancy_id: int):
+    vacancies = read_data(VACANCY_FILE)
+
+    for vac in vacancies:
+        if vac.get("id") == vacancy_id:
+            if "img" in vac:
+                vac["img"] = f"{IMAGE_URL}/{vac['img']}"
+            return vac
+
+    return {"error": f"Vacancy with id {vacancy_id} not found"}
+
 
 
 
